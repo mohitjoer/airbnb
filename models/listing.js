c7d8fd2f1580c85478525ae511a9e698ffc699d7
mohitjoer <mohitjoe.r@gmail.com>
2025-03-24 14:07:22 +0530

    commit	modified:   models/listing.js

--- models/listing.js
+++ models/listing.js
@@ -7,6 +7,7 @@ const listingschema =new schema({
     discription:{type :String,
             required:true},
     image:{ type :String,
+            default:'https://images.unsplash.com/photo-1742657914528-31c194363a16?q=80&w=709&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             set:(v)=> v===""?"https://images.unsplash.com/photo-1742657914528-31c194363a16?q=80&w=709&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,},
     price:{ type :Number,
             required:true},
