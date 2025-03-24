84849cc2b549a1fb61c69f35f0f410b18d7c6150
mohitjoer <mohitjoe.r@gmail.com>
2025-03-24 14:31:22 +0530

    commit	new file:   init/data.js commit	new file:   init/index.js commit	modified:   models/listing.js

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const initdata = require("./data.js");
+const listing = require("../models/listing.js");
+
+
+const mongo_url = 'mongodb://127.0.0.1:27017/wanderl';
+main()
+.then(()=>{
+    console.log("connected to DB");
+}).catch(err=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(mongo_url);
+}
+
+const initdb = async () =>{
+    await listing.deleteMany({});
+    await listing.insertMany(initdata.data);
+    console.log("data was initialized");
+
+};
+
+initdb();
--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,38 @@
-const mongoose = require('mongoose')
-const schema =mongoose.Schema;
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
 
 const listingschema =new schema({
-    title:{ type: String,
-            required:true},
-    discription:{type :String,
-            required:true},
-    image:{ type :String,
-            default:'https://images.unsplash.com/photo-1742657914528-31c194363a16?q=80&w=709&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            set:(v)=> v===""?"https://images.unsplash.com/photo-1742657914528-31c194363a16?q=80&w=709&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,},
-    price:{ type :Number,
-            required:true},
-    location:{ type :String,
-            required:true},
-    country:{ type :String,
-            required:true},
+        title: { 
+                type: String,
+                required: true
+            },
+        description: {
+                type: String,
+                required: true
+            },
+        image: {
+                filename: {
+                    type: String,
+                    required: true
+                },
+                url: {
+                    type: String,
+                    required: true,
+                    default: 'https://images.unsplash.com/photo-1742657914528-31c194363a16?q=80&w=709&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                }
+            },
+        price: {
+                type: Number,
+                required: true
+            },
+        location: {
+                type: String,
+                required: true
+            },
+        country: {
+                type: String,
+                required: true
+            }
 });
 
 const listing= mongoose.model("listing", listingschema );
